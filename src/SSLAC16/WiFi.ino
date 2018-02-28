eb8b1f56b760dcbaa73ee3f48dc07adc8225abb8
AndrewReloaded <andrew_my@inbox.ru>
2018-02-28 20:13:32 +0300

    Refactoring

--- src/SSLAC16/WiFi.ino
+++ src/SSLAC16/WiFi.ino
@@ -11,16 +11,13 @@ void setupWiFi()
   
   WiFi.mode(WIFI_AP_STA);
   
-  //isConn = false;
-  //if (foundWiFi(ssid)) 
-  //{
-    connectWiFi();
-  //}   
+  connectWiFi();  
 }
 
 void connectWiFi()
 {
-  isConn = false;
+  //ssid = "home";
+  //password = "";
   
   WiFi.begin(ssid.c_str(), password.c_str());
 
@@ -29,15 +26,20 @@ void connectWiFi()
   {
     delay(500);
     tmp_a++;
-    //isConn = true;
     if (tmp_a > 128) 
     {
-      //isConn = false;
       break;
     }
   }
   
   isConn = WiFi.localIP() != 0;
+
+  if(isConn)
+  {
+    Serial.println("WiFi connected");
+    Serial.print(F("WiFi IP address: "));
+    Serial.println(WiFi.localIP());
+  }
 }
 
 void reconnectWiFi()
@@ -57,6 +59,7 @@ void startSoftAP()
   String SSID_AP = esp_hostname;
   SSID_AP += "_";
   String PASSWORD_AP;
+  
   if (ESP.getChipId() < 10000000) 
   {
     SSID_AP += String(10000000 + ESP.getChipId());
@@ -78,32 +81,10 @@ void startSoftAP()
   }
 
   WiFi.softAP(SSID_AP.c_str(), PASSWORD_AP.c_str());
-  Serial.print(F("Starting AP new : ")); Serial.print(SSID_AP); Serial.print("/"); Serial.println(PASSWORD_AP);
-  IPAddress myIP = WiFi.softAPIP();
+  Serial.print(F("Starting soft AP: ")); Serial.print(SSID_AP); Serial.print("/"); Serial.println(PASSWORD_AP);
   Serial.print(F("AP IP address: "));
-  Serial.println(myIP);
+  Serial.println(WiFi.softAPIP());
 }
 
-bool foundWiFi(String _SSID) 
-{
-  foundedNet = WiFi.scanNetworks();
-  bool result = false;
-  if (foundedNet == 0) 
-  {
-    result = false;
-  } 
-  else 
-  {
-    for (int i = 0; i < foundedNet; ++i) 
-    {
-      if (_SSID == WiFi.SSID(i)) 
-      {
-        result = true;
-        break;
-      }
-    }
-  }
-  return result;
-}
 
 
